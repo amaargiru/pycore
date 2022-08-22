32be2c813bf83104d30d819369d93db092709850
amaargiru <war4one@gmail.com>
2022-08-22 18:00:40 +0500

    Change project's structure

--- compose.bat
+++ compose.bat
@@ -1,3 +1,4 @@
 jupyter nbconvert --to markdown 01_python.ipynb --output "01_python.md"
 jupyter nbconvert --to markdown 04_algorithms.ipynb --output "04_algorithms.md"
+copy 01_python.md + 02_postgre.md + 03_architecture.md + 04_algorithms.md + 05_admin_devops.md + 06_pytest.md + 07_fastapi.md + 08_flask.md readme.md
 pause
