b1d501480f5a49887c9431b67037ac4964f34175
amaargiru <war4one@gmail.com>
2023-04-09 19:45:42 +0500

    Update project structure

--- compose_readme.bat
+++ compose_readme.bat
@@ -1,3 +1,4 @@
+jupyter nbconvert --to markdown 00_intro.ipynb --output 00_intro.md
 jupyter nbconvert --to markdown 01_data_structures.ipynb --output 01_data_structures.md
 jupyter nbconvert --to markdown 02_data_management.ipynb --output 02_data_management.md
 jupyter nbconvert --to markdown 03_data_flows.ipynb --output 03_data_flows.md
@@ -8,9 +9,14 @@ jupyter nbconvert --to markdown 07_common_practice.ipynb --output 07_common_prac
 jupyter nbconvert --to markdown 08_algorithms.ipynb --output 08_algorithms.md
 jupyter nbconvert --to markdown 09_database.ipynb --output 09_database.md
 jupyter nbconvert --to markdown 10_net.ipynb --output 10_net.md
+jupyter nbconvert --to markdown 11_architecture.ipynb --output 11_architecture.md
+jupyter nbconvert --to markdown 12_admin_devops.ipynb --output 12_admin_devops.md
+jupyter nbconvert --to markdown 13_outro.ipynb --output 13_outro.md
 
 copy 00_intro.md + 01_data_structures.md + 01_remark.md + 02_data_management.md + 03_data_flows.md + 04_oop.md + 05_language_skeleton.md + 06_multithreading_multiprocessing.md + 07_common_practice.md + 08_algorithms.md + 09_database.md + 10_net.md + 11_architecture.md + 12_admin_devops.md + 13_outro.md readme.md
 
+
+del 00_intro.md
 del 01_data_structures.md
 del 02_data_management.md
 del 03_data_flows.md
@@ -18,10 +24,12 @@ del 04_oop.md
 del 05_language_skeleton.md
 del 06_multithreading_multiprocessing.md
 del 07_common_practice.md
-
 del 08_algorithms.md
 del 09_database.md
 del 10_net.md
+del 11_architecture.md
+del 12_admin_devops.md
+del 13_outro.md
 
 rem Delete file from "Context manager" chapter
 del test_context_connect.db
