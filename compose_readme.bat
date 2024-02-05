6f6babc98f5ed875ce575e488eafbf338953be8e
Mihail Emelyanov <war4one@gmail.com>
2024-02-05 16:14:32 +0500

    Add leetcode questions

--- compose_readme.bat
+++ compose_readme.bat
@@ -13,8 +13,10 @@ jupyter nbconvert --to markdown 11_architecture.ipynb --output 11_architecture.m
 jupyter nbconvert --to markdown 12_admin_devops.ipynb --output 12_admin_devops.md
 jupyter nbconvert --to markdown 13_outro.ipynb --output 13_outro.md
 jupyter nbconvert --to markdown 14_annex_1_interview_questions.ipynb --output 14_annex_1_interview_questions.md
+jupyter nbconvert --to markdown 15_annex_2_leetcode_questions.ipynb --output 15_annex_2_leetcode_questions.md
 
-copy 00_intro.md + 01_data_structures.md + 01_remark.md + 02_data_management.md + 03_data_flows.md + 04_oop.md + 05_language_skeleton.md + 06_multithreading_multiprocessing.md + 07_common_practice.md + 08_algorithms.md + 09_database.md + 10_net.md + 11_architecture.md + 12_admin_devops.md + 13_outro.md  + 14_annex_1_interview_questions.md readme.md
+
+copy 00_intro.md + 01_data_structures.md + 01_remark.md + 02_data_management.md + 03_data_flows.md + 04_oop.md + 05_language_skeleton.md + 06_multithreading_multiprocessing.md + 07_common_practice.md + 08_algorithms.md + 09_database.md + 10_net.md + 11_architecture.md + 12_admin_devops.md + 13_outro.md  + 14_annex_1_interview_questions.md + 15_annex_2_leetcode_questions.md readme.md
 
 
 del 00_intro.md
@@ -32,6 +34,7 @@ del 11_architecture.md
 del 12_admin_devops.md
 del 13_outro.md
 del 14_annex_1_interview_questions.md
+del 15_annex_2_leetcode_questions.md
 
 rem Delete file from "Context manager" chapter
 del test_context_connect.db
